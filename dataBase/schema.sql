9b22214032f2e1c0f12e80d62222aa600d88b58b
Yuriy Kuptsov <yuriy.kuptsov@gmail.com>
2021-06-04 11:46:17 -0400

    Data added

--- dataBase/schema.sql
+++ dataBase/schema.sql
@@ -9,3 +9,10 @@ CREATE TABLE dogsTable (
   description varchar(200) NOT NULL,
   PRIMARY KEY (ID)
 );
+-- CREATE TABLE catsTable (
+
+--   id int NOT NULL AUTO_INCREMENT,
+--   name varchar(20) NOT NULL,
+--   description varchar(200) NOT NULL,
+--   PRIMARY KEY (ID)
+-- );
